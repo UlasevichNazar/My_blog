21d06f9953fa4f9a21c94f19c411b6cd3add16f6
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-05-23 18:42:10 +0300

    feat: create new model (Post) and change admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.urls import reverse
+from blog.models import Post
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author_link', 'created', 'status']
+    list_filter = ['status', 'created', 'published', 'author']
+    search_fields = ['title', 'author']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
+    autocomplete_fields = ('author',)
+
+    def author_link(self, obj):
+        user = obj.author
+        link = reverse('admin:auth_user_change', args=[user.pk])
+        return format_html('<a href="{}">{}</a>', link, user.get_username())
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    author = models.ForeignKey(User, models.CASCADE, related_name='blog_post', verbose_name='Author')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published'])
+        ]
+
+    def __str__(self):
+        return self.title
