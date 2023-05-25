756fd2cde9b91ec80f1d7b89f02d24cca7ea87c1
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-05-25 20:16:12 +0300

    feat: create post_list and post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    post = Post.published.all()
+    return render(request, 'blog/post/list.html', {'post': post})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
+    # try:
+    #     post=Post.published.get(pk=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found")
+
+    return render(request, 'blog/post/detail.html', {'post': post})
