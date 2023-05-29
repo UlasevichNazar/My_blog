33b493d91c2d646a7117675a678ce6c06428961f
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-05-29 22:44:54 +0300

    feat: create email sending

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
-def post_list(request):
-    post = Post.published.all()
-    paginator = Paginator(post, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     post = Post.published.all()
+#     paginator = Paginator(post, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -22,3 +30,23 @@ def post_detail(request, year, month, day, post):
                              publish__month=month, publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            success_form = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{success_form['name']} recommends you read"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{success_form['name']}\'s comments: {success_form['comments']}"
+            send_mail(subject, message, 'ulnazar51@gmail.com', [success_form['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form,
+                                                    'sent': sent})
--- config/settings.py
+++ config/settings.py
@@ -92,3 +92,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#############################################
+# SMTP
+#############################################
+EMAIL_HOST = env.str('EMAIL_HOST')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
